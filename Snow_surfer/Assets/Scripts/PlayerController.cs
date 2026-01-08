a87d49885d13efc4a94c6f49893c4e43b1bd9dc0
OrkhanElchuev <orkhan.elchuev@gmail.com>
2026-01-08 02:00:40 +0100

    Add Left and Right Rotation, Adjust Speed and Torque

--- Snow_surfer/Assets/Scripts/PlayerController.cs
+++ Snow_surfer/Assets/Scripts/PlayerController.cs
@@ -3,17 +3,28 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float torqueAmount = 1f;
     InputAction moveAction;
+    Rigidbody2D myRigidBody2D;
 
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        myRigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         Vector2 moveVector;
         moveVector = moveAction.ReadValue<Vector2>();
-        print(moveVector);
+        
+        if (moveVector.x < 0)
+        {
+            myRigidBody2D.AddTorque(torqueAmount);
+        }
+        else if (moveVector.x > 0)
+        {
+            myRigidBody2D.AddTorque(-torqueAmount);
+        }
     }
 }
