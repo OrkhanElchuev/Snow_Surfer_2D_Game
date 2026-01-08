323e0a0d3153218fa6d75a0d62b66e7edcf6c3b7
OrkhanElchuev <orkhan.elchuev@gmail.com>
2026-01-08 19:30:05 +0100

    Add Sliding particle, Add Speed Boost via W key

--- Snow_surfer/Assets/Scripts/PlayerController.cs
+++ Snow_surfer/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,35 @@
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float baseSpeed = 15f;
+    [SerializeField] float boostSpeed = 20f;
+
     InputAction moveAction;
     Rigidbody2D myRigidBody2D;
+    SurfaceEffector2D surfaceEffector2D;
+
+    Vector2 moveVector;
+
 
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         myRigidBody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
     }
 
     void Update()
     {
-        Vector2 moveVector;
+        RotatePlayer();
+        BoostPlayer();
+    }
+
+    void RotatePlayer()
+    {
         moveVector = moveAction.ReadValue<Vector2>();
         
         if (moveVector.x < 0)
@@ -25,6 +39,18 @@ public class PlayerController : MonoBehaviour
         else if (moveVector.x > 0)
         {
             myRigidBody2D.AddTorque(-torqueAmount);
+        }
+    }
+
+    void BoostPlayer()
+    {
+        if (moveVector.y > 0)
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
         }
     }
 }
